0a5e923463d9aee4598488623b782d123dbec1f9
Veronica De Felice <verodefelic@gmail.com>
2023-03-27 22:15:55 -0400

    Week 2 Repo

---
+++ homework/dayOneWeekTwo.js
@@ -0,0 +1,26 @@
+/////////HOMEWORK #1
+
+
+let myCar = {brand: "Toyota", make: "Prius", color: "red"}
+
+const  {color} = myCar
+
+console.log (color)
+
+
+/////////////HOMEWORK #2
+
+
+function convInSec (hours){
+
+
+    return seconds = 60 * hours
+
+}
+
+console.log(convInSec(2))
+
+
+
+
+
