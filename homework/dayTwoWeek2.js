3975c36975d10aeec56b510c8b09a4f49d44b409
Veronica De Felice <verodefelic@gmail.com>
2023-03-29 20:09:00 -0400

    Homework Day2 Week2

---
+++ homework/dayTwoWeek2.js
@@ -0,0 +1,14 @@
+
+////HOMEWORK #1
+
+function totalScore(points2, points3){
+
+    const twoPointers = points2 * 2
+    const threePointers = points3 * 3
+    return result = twoPointers + threePointers
+
+}
+    
+    totalScore(4, 2)
+    console.log(result)
+    
